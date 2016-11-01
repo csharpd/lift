507114e14a00c86d8914629e6845e2cf610ae60f
Chloe Donegan <chloedonegan@CDONEGANQU402.local>
2016-11-01 12:03:30 +0000

    ready for CSE School

--- app/index.js
+++ app/index.js
@@ -1,21 +1,21 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+// var React = require('react');
+// var ReactDOM = require('react-dom');
 
 
-var HelloWorld = React.createClass({
-	render: function () {
-		console.log(this.props)
-		return (
-			<div>Hello WORLD</div>
-		)
-	}
-})
+// var HelloWorld = React.createClass({
+// 	render: function () {
+// 		console.log(this.props)
+// 		return (
+// 			<div>Hello WORLD</div>
+// 		)
+// 	}
+// })
 
 
-ReactDOM.render(
-	<HelloWorld name="Sales Engineers" />,
-	document.getElementById('app')
-)
+// ReactDOM.render(
+// 	<HelloWorld name="Engineers" />,
+// 	document.getElementById('app')
+// )
 
 
 // ** Second **
@@ -39,7 +39,7 @@ ReactDOM.render(
 
 
 // ReactDOM.render(
-// 	<HelloWorld name="Sales Engineers"company="Qubit" employees={150}/>,
+// 	<HelloWorld name="Engineers" company="Qubit" employees={150}/>,
 // 	document.getElementById('app')
 // )
 
@@ -98,71 +98,72 @@ ReactDOM.render(
 
 
 // ** FINAL **
-// var USER_DATA = {
-// 	name: "chloe",
-// 	username: 'csharpd',
-// 	image: 'https://avatars3.githubusercontent.com/u/4848969?v=3&u=c8690a420ccfd7a5ea4f4028bfc479859177e854&s=140'
-// }
+var USER_DATA = {
+	name: "chloe",
+	username: 'csharpd',
+	image: 'https://avatars3.githubusercontent.com/u/4848969?v=3&u=c8690a420ccfd7a5ea4f4028bfc479859177e854&s=140'
+}
 
-// var React = require('react');
-// var ReactDOM = require('react-dom');
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-// var ProfilePic = React.createClass({
-// 	render: function () {
-// 		return <img src={this.props.imageUrl} style={{height: 100, width: 100}}></img>
-// 	}
-// });
+var ProfilePic = React.createClass({
+	render: function () {
+		return <img src={this.props.imageUrl} style={{height: 100, width: 100}}></img>
+	}
+});
 
-// // whatever the component wraps will become {this.props.children}
-// // in this case the <Link> component nested in Profile Link wraps {this.props.username}
-// // we can call this.changeURL in render as it is a function defined within the Link component
-// var Link = React.createClass({
-// 	changeURL: function () {
-// 		window.location.replace(this.props.href)
-// 	},
-// 	render: function () {
-// 		console.log(this.props.children);
-// 		return (
-// 			<span 
-// 				style={{color: 'blue', cursor: 'pointer'}}
-// 				onClick={this.changeURL}>
-// 				{this.props.children}
-// 			</span>
-// 		)
-// 	}
-// })
+var ProfileName = React.createClass({
+	render: function () {
+		return <div>{this.props.name}</div>
+	}
+})
 
-// var ProfileLink = React.createClass({
-// 	render: function () {
-// 		return (
-// 			<div>
-// 				<Link href={'https://www.github.com/' + this.props.username}>
-// 					{this.props.username}
-// 				</Link>
-// 			</div>
-// 		)
-// 	}
-// });
+var ProfileLink = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<Link href={'https://www.github.com/' + this.props.username}>
+			
+					cats
+				</Link>
+			</div>
+		)
+	}
+});
 
-// var ProfileName = React.createClass({
-// 	render: function () {
-// 		return <div>{this.props.name}</div>
-// 	}
-// })
+// whatever the component wraps will become {this.props.children}
+// in this case the <Link> component nested in Profile Link wraps {this.props.username}
+// we can call this.changeURL in render as it is a function defined within the Link component
+var Link = React.createClass({
+	changeURL: function () {
+		window.location.replace(this.props.href)
+	},
+	render: function () {
+		console.log(this.props.children);
+		return (
+			<span 
+				style={{color: 'blue', cursor: 'pointer'}}
+				onClick={this.changeURL}>
+				{this.props.children}
+			</span>
+		)
+	}
+})
 
-// var Avatar = React.createClass({
-//   render: function () {
-//     return(
-//       <div>
-//         <ProfilePic imageUrl={this.props.user.image}/>
-//         <ProfileName name={this.props.user.name}/>
-//         <ProfileLink username={this.props.user.username}/>
-//       </div>
-//     );
-//   }
-// });
+var Avatar = React.createClass({
+  render: function () {
+    return(
+      <div>
+        <ProfilePic imageUrl={this.props.user.image}/>
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
+      </div>
+    );
+  }
+});
 
-// ReactDOM.render(
-// 	<Avatar user={USER_DATA}/>,
-// 	document.getElementById('app')
-// )
+ReactDOM.render(
+	<Avatar user={USER_DATA}/>,
+	document.getElementById('app')
+)
