2f043b64fbc52c8dea3957630e8c74ca1b8be736
Chloe Donegan <chloedonegan@CDONEGANQU402.local>
2016-11-01 16:11:44 +0000

    Submit location

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,13 +4,20 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var hashHistory = ReactRouter.hashHistory;
 var IndexRoute = ReactRouter.IndexRoute;
-var Main = require('../containers/main');
-var Home = require('../components/Home');
+var Main = require('../containers/Main');
+var HomeContainer = require('../containers/HomeContainer')
 
+// define a route inside of the router with a path and a component property.
+// the Main component will always be active i.e like a consistant header/footer that is present universally.
+// home is a child route of main. 
+// it becomes active when /home.
+// <Route path='/home' component={Home}>
+// <IndexRoute> would define the default route if no other /xyz are active
+// History is the default react router history.
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
-    	<IndexRoute component={Home} />
+      <IndexRoute component={HomeContainer} />
     </Route>
   </Router>
 );
